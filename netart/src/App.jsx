8aa7deac9a826d11e54f4133f6c98bff6d5e8251
saurabhpushpam <145956198+saurabhpushpam@users.noreply.github.com>
2024-02-27 23:58:32 +0530

    commit

--- netart/src/App.jsx
+++ netart/src/App.jsx
@@ -3,13 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Pages from './components/Pages'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 
 function App() {
 
   return (
     <>
-      <Pages></Pages>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Pages></Pages>}></Route>
+        </Routes>
+      </BrowserRouter>
+
     </>
   )
 }
